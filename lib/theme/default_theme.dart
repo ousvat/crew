ebb95e9a4a8d482e33db3380247f5ddf652235db
ovidiuusvat <ovidiu.usvat@techstackapps.com>
2021-02-01 09:57:43 +0200

    Import Icons and Fonts

---
+++ lib/theme/default_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+ThemeData defaultTheme = ThemeData(
+    primaryColor: Color(0xFF071B33),
+    disabledColor: Color(0xFF8795A5),
+    accentColor: Color(0xFFE21918),
+    fontFamily: GoogleFonts.roboto().fontFamily,
+    );
