d0d1a3e66fe7ef645092e7c46394d527b803faa5
ovidiuusvat <ovidiu.usvat@techstackapps.com>
2021-02-01 10:29:37 +0200

    Splach screen implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:Crew/screens/00-Splash-Screen/splash_screen.dart';
 import 'package:Crew/theme/default_theme.dart';
 import 'package:flutter/material.dart';
 
@@ -13,41 +14,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: defaultTheme,
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              'Hello',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
+      home: SplashScreen(),
     );
   }
 }
